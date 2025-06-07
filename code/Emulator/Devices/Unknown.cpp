afc2ad3cd68164480be27013f469c4c93f8bb7a9
Anders Pistol <anders.pistol@doctorentertainment.com>
2025-06-07 23:32:51 +0200

    Added "Unknown" device to emulator.

---
+++ code/Emulator/Devices/Unknown.cpp
@@ -0,0 +1,26 @@
+/*
+ Klara-RTL
+ Copyright (c) 2025 Anders Pistol.
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+#include "Emulator/Devices/Unknown.h"
+
+T_IMPLEMENT_RTTI_CLASS(L"Unknown", Unknown, IDevice)
+
+bool Unknown::writeU32(uint32_t address, uint32_t value)
+{
+	return true;
+}
+
+uint32_t Unknown::readU32(uint32_t address) const
+{
+	return 0;
+}
+
+bool Unknown::tick(CPU* cpu)
+{
+	return true;
+}
