0cbd19f95472a2810055fe747fea2be5cde1f8c9
Anders Pistol <anders.pistol@doctorentertainment.com>
2025-08-24 15:43:01 +0200

    Some minor work to GDB server.

--- code/Emulator2/CPU/GDBServer.cpp
+++ code/Emulator2/CPU/GDBServer.cpp
@@ -8,7 +8,11 @@
 */
 #include "Emulator2/CPU/GDBServer.h"
 
+#include <Core/Io/StringReader.h>
+#include <Core/Io/Utf8Encoding.h>
 #include <Core/Log/Log.h>
+#include <Core/Misc/String.h>
+#include <Core/Misc/TString.h>
 #include <Net/SocketAddressIPv4.h>
 #include <Net/SocketSet.h>
 #include <Net/SocketStream.h>
@@ -16,6 +20,28 @@
 using namespace traktor;
 
 T_IMPLEMENT_RTTI_CLASS(L"GDBServer", GDBServer, Object)
+
+namespace
+{
+
+	void send(net::TcpSocket* socket, const std::string& msg)
+	{
+		const char hex[] = "0123456789ABCDEF";
+		uint8_t cs = 0;
+
+		socket->send('+');
+		socket->send('$');
+		for (const char ch : msg)
+		{
+			socket->send(ch);
+			cs += (uint8_t)ch;
+		}
+		socket->send('#');
+		socket->send(hex[cs >> 4]);
+		socket->send(hex[cs & 15]);
+	}
+
+}
 
 bool GDBServer::create()
 {
@@ -33,10 +59,44 @@ void GDBServer::process()
 	if (m_clientSocket)
 	{
 		// Receive commands from GDB.
+		if (m_clientSocket->select(true, false, false, 0) > 0)
+		{
+			char ch;
+			m_clientSocket->recv(&ch, 1);
 
-		net::SocketStream clientStream(m_clientSocket, true, true, 100);
+			if (ch == '$')
+			{
+				// Receive message until end character.
+				std::string msg;
+				for (;;)
+				{
+					m_clientSocket->recv(&ch, 1);
+					if (ch == '#')
+						break;
 
-		
+					msg += ch;
+				}
+
+				// Read checksum.
+				m_clientSocket->recv(&ch, 1);
+				m_clientSocket->recv(&ch, 1);
+
+				log::info << L"GDB, got message \"" << mbstows(msg) << L"\"" << Endl;
+
+				// Process message.
+				if (msg == "?")
+					send(m_clientSocket, "S05");
+				else if (msg == "c")
+					send(m_clientSocket, "S05");
+				else if (startsWith(msg, "qSupported"))
+					send(m_clientSocket, "");
+				else
+					send(m_clientSocket, "");
+			}
+
+			
+		}
+
 	}
 	else
 	{
