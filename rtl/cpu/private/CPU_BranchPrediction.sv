6c25594d798e47906d7025130faa14c8b64c6132
Anders Pistol <anders.pistol@doctorentertainment.com>
2026-02-18 20:10:00 +0100

    Cleaned up the BP unit.

--- rtl/cpu/private/CPU_BranchPrediction.sv
+++ rtl/cpu/private/CPU_BranchPrediction.sv
@@ -12,6 +12,7 @@
 `default_nettype none
 
 module CPU_BranchPrediction (
+	input wire i_reset,
 	input wire i_clock,
 	input wire [31:0] i_pc,
 
@@ -26,46 +27,36 @@ module CPU_BranchPrediction (
 	input wire i_jump,
 	input wire [31:0] i_jump_pc
 );
-	bit [22:0] from_pc [64];
-	bit [31:0] target_pc [64];
+	bit [26:0] from_pc [8];
+	bit [31:0] target_pc [8];
 
-	genvar i;
-	generate for (i = 0; i < 64; i = i + 1) begin : initialize_btb
-		initial from_pc[i] = 0;
-		initial target_pc[i] = 0;
-	end endgenerate
+	wire [2:0] i_pc_tag = i_pc[4:2];
+	wire [26:0] i_pc_rest = i_pc[31:5];
 
-	wire [5:0] i_pc_tag = i_pc[8:2];
-	wire [22:0] i_pc_rest = i_pc[31:9];
-
-	wire [5:0] i_pc_launch_tag = i_pc_launch[8:2];
-	wire [22:0] i_pc_launch_rest = i_pc_launch[31:9];
+	wire [2:0] i_pc_launch_tag = i_pc_launch[4:2];
+	wire [26:0] i_pc_launch_rest = i_pc_launch[31:5];
 
 	always_comb begin
 		o_pc_hint = i_pc;
-
 		if (from_pc[i_pc_tag] == i_pc_rest)
 			o_pc_hint = target_pc[i_pc_tag];
 		else if (i_is_jal)
 			o_pc_hint = i_pc + i_inst_J_imm;
 		else if (i_is_jump_conditional)
 			o_pc_hint = i_pc + i_inst_B_imm;
-
 	end
 
-	bit [31:0] dbg_bp_hit = 0;
-	bit [31:0] dbg_bp_miss = 0;
-
 	always_ff @(posedge i_clock) begin
-		if (i_jump) begin
-			if (i_jump_pc == i_pc)
-				dbg_bp_hit <= dbg_bp_hit + 1;
-			else begin
-				dbg_bp_miss <= dbg_bp_miss + 1;
-
+		if (i_reset) begin
+			for (integer i = 0; i < 8; i = i + 1) begin
+				from_pc[i] <= 0;
+				target_pc[i] <= 0;
+			end
+		end
+		else begin
+			if (i_jump && i_jump_pc != i_pc) begin
 				from_pc[i_pc_launch_tag] <= i_pc_launch_rest;
 				target_pc[i_pc_launch_tag] <= i_jump_pc;
-
 			end
 		end
 	end
